8cd0e7d143ce3dbfa3a0ec29c69936396d58c8a6
valterekarina <101064793+valterekarina@users.noreply.github.com>
2024-02-26 13:30:04 +0200

    code cleanup

--- TechnicalTaskQaA_API/Program.cs
+++ TechnicalTaskQaA_API/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+ 
 builder.Services.AddDbContext<AppDbContext_API>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
